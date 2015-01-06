5bf8b644608bbca252aafd7dc574c1a0a1bca916
AMESRobotics2015 <ames.amperes@gmail.com>
2015-01-05 18:15:48 -0700

    Final tests

--- RobotCode2015/src/edu/wpi/first/wpilibj/templates/RobotMain.java
+++ RobotCode2015/src/edu/wpi/first/wpilibj/templates/RobotMain.java
@@ -25,7 +25,7 @@ public class RobotMain extends IterativeRobot {
     public void robotInit() {
 
     }
-
+//Testing one two three
     /**
      * This function is called periodically during autonomous
      */
